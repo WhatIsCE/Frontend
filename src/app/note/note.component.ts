7db44267d358b8d116a350273191146352a7b7de
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2018-06-10 23:22:57 +0430

    added sample model

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from '../note';
+import { NOTES } from '../mock-notes';
 
 @Component({
-  selector: 'app-note',
-  templateUrl: './note.component.html',
-  styleUrls: ['./note.component.css']
+  selector: "app-note",
+  templateUrl: "./note.component.html",
+  styleUrls: ["./note.component.css"]
 })
 export class NoteComponent implements OnInit {
 
+  notes: Note[] = NOTES;
+
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
