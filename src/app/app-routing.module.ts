61832aebc0e98a0e699d8a9a3652cb1079c10d0c
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2018-06-11 11:17:09 +0430

    added functionality to site nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
+import { NoteComponent } from "./note/note.component";
+
+const routes: Routes = [
+  { path: "", component: NoteComponent }
+  //{ path: 'about', component: aboutComponent },
+  //{ path: 'contact', component: contactComponent }
+];
 
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+
+}
