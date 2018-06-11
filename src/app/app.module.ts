1a95e5fbf036d21cc4cec7443c19a91d6ce6a9ff
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2018-06-11 11:07:30 +0430

    generated app routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NoteComponent } from './note/note.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { NoteComponent } from './note/note.component';
     NoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
