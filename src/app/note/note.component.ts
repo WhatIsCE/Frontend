ff0377647d75d45b759d2546ad243cdf4c5d5103
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2018-06-11 11:50:52 +0430

    added text changer

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -20,4 +20,17 @@ export class NoteComponent implements OnInit {
     this.noteService.getNotes()
       .subscribe(notes => { this.notes = notes; this.isLoaded = true; console.log(this.notes) });
   }
+
+  nextText() {
+    if (this.selectedIndex < this.notes.length) {
+      setTimeout(this.nextText, this.calculateTime())
+      this.selectedIndex++;
+    } else {
+      this.selectedIndex = 0;
+    }
+  }
+
+  private calculateTime(): number {
+    return this.notes[this.selectedIndex].content.length / 25 * 1000;
+  }
 }
